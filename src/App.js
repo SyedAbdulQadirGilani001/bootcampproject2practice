76b179251ecf783f03edc8f7c38660741bf537ec
SyedAbdulQadirGilani001 <66156655+SyedAbdulQadirGilani001@users.noreply.github.com>
2020-06-06 13:05:50 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is the experimental page of react app.
+          Hello from Abdul qadir.
         </p>
         <a
           className="App-link"
